7de517ca5f220913bb128881005665f8c0128fbc
Jeffrey Huang <jyhuang2005@gmail.com>
2026-03-09 17:33:47 -0400

    Basic structure

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+def process_three_points():
+    """
+    Processes a single set of three points
+    """
+
+
+def calculate_area(points):
+    """
+    Iterates over sets of three points along curve
+    """
+
+
+def process_data():
+    """
+    Reads and formats data
+    Returns an array of triplets [(x1, y1, z1), (x2, y2, z2), ...]
+    """
+    return []
+
+
 print("Hello World")
+points = process_data()
+calculate_area(points)
